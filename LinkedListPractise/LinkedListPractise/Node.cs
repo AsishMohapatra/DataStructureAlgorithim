3b2e3363d4a79d272db3587aa3f75329b93f576a
Asish <mohapatra.asish@gmail.com>
2021-11-29 22:53:23 +0800

    Initializing Linked List and printing a linked list

---
+++ LinkedListPractise/LinkedListPractise/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedListPractise
+{
+    public class Node<T>
+    {
+        public T Data { get; set; }
+        public Node<T> NextNode{ get; set; }
+
+        public Node(T data)
+        {
+            this.Data = data;
+            this.NextNode = null;
+        }
+    }
+}
--- LinkedListPractise/LinkedListPractise/Program.cs
+++ LinkedListPractise/LinkedListPractise/Program.cs
@@ -7,6 +7,27 @@ namespace LinkedListPractise
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var head=InitialiseNodesInALinkedList();
+            PrintLinkedList(head);
+        }
+
+        public static Node<int> InitialiseNodesInALinkedList()
+        {
+            Node<int> head = new Node<int>(5);
+            head.NextNode = new Node<int>(8);
+            head.NextNode.NextNode = new Node<int>(15);
+            head.NextNode.NextNode.NextNode = new Node<int>(29);
+            return head;
+        }
+
+        public static void PrintLinkedList(Node<int> headNode)
+        {
+            var currentNode = headNode;
+            while (currentNode != null)
+            {
+                Console.WriteLine($"Node data ::  {currentNode.Data}");
+                currentNode = currentNode.NextNode;
+            }
         }
     }
 }
