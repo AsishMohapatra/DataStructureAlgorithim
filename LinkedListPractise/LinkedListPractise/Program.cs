d519242c9cc078aeef40ec2a29044404a1ed3a9d
Asish <mohapatra.asish@gmail.com>
2021-11-30 23:25:33 +0800

    InsertAtIndex and Search in the linked list

--- LinkedListPractise/LinkedListPractise/Program.cs
+++ LinkedListPractise/LinkedListPractise/Program.cs
@@ -7,10 +7,10 @@ namespace LinkedListPractise
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            
-            var head=InitialiseNodesInALinkedList();
+
+            var head = InitialiseNodesInALinkedList();
             //head = DeleteHead(head);
-            head=InsertAtBeginning(head, 40);
+            head = InsertAtBeginning(head, 40);
             head = InsertAtBeginning(head, 30);
             head = InsertAtEnd(head, 95);
             head = InsertAtBeginning(head, 10);
@@ -19,18 +19,34 @@ namespace LinkedListPractise
             head = DeleteHead(head);
             PrintLinkedListUsingrecursion(head);
             head = DeleteTail(head);
-            head = DeleteTail(head);
-            head = DeleteTail(head);
-            head = DeleteTail(head);
-            head = DeleteTail(head);
-            head = DeleteTail(head);
-            head = DeleteTail(head);
-            head = DeleteTail(head);
-            head = DeleteTail(head);
-            head = DeleteTail(head);
 
             //PrintLinkedList(head);
             PrintLinkedListUsingrecursion(head);
+
+            head = InsertDataAtIndex(head, 1, 10);
+            PrintLinkedListUsingrecursion(head);
+
+            head = InsertDataAtIndex(head, 8, 100);
+            PrintLinkedListUsingrecursion(head);
+
+            head = InsertDataAtIndex(head, 5, 75);
+            PrintLinkedListUsingrecursion(head);
+
+            head = InsertDataAtIndex(head, 15, 1000);
+            PrintLinkedListUsingrecursion(head);
+
+            var containsData = Contains(head, 10);
+            containsData = Contains(head, 105);
+            containsData = Contains(head, 30);
+            containsData = Contains(head, 100);
+            containsData = Contains(head, 75);
+
+            var containsDataUsingIteration = ContainsIterative(head, 10);
+            containsDataUsingIteration = ContainsIterative(head, 105);
+            containsDataUsingIteration = ContainsIterative(head, 30);
+            containsDataUsingIteration = ContainsIterative(head, 40);
+            containsDataUsingIteration = ContainsIterative(head, 100);
+            containsDataUsingIteration = ContainsIterative(head, 75);
 
         }
 
@@ -68,13 +84,16 @@ namespace LinkedListPractise
         public static void PrintLinkedListUsingrecursion(Node<int> headNode)
         {
             if (headNode == null)
+            {
+                Console.WriteLine();
                 return;
+            }
             Console.Write($"{headNode.Data} ");
             PrintLinkedListUsingrecursion(headNode.NextNode);
-            Console.WriteLine();
+
         }
 
-        public static Node<int> InsertAtBeginning(Node<int> headNode,int x)
+        public static Node<int> InsertAtBeginning(Node<int> headNode, int x)
         {
             Node<int> newHead = new Node<int>(x);
             newHead.NextNode = headNode;
@@ -109,7 +128,7 @@ namespace LinkedListPractise
         public static Node<int> DeleteTail(Node<int> headNode)
         {
             Console.WriteLine("Deleting tail item");
-            if (headNode == null || headNode.NextNode==null) return null;
+            if (headNode == null || headNode.NextNode == null) return null;
             var currentNode = headNode;
             Node<int> previousNode = null;
             while (currentNode.NextNode != null)
@@ -121,5 +140,66 @@ namespace LinkedListPractise
             return headNode;
         }
 
+        public static Node<int> InsertDataAtIndex(Node<int> head, int index, int data)
+        {
+            Console.WriteLine($"Inserting {data} at index {index}");
+            var dataNode = new Node<int>(data);
+            if (index == 1)
+            {
+                dataNode.NextNode = head;
+                return dataNode;
+            }
+
+            var currentNode = head;
+            int currentPosition = 1;
+            while (currentNode.NextNode != null && currentPosition < index)
+            {
+                currentNode = currentNode.NextNode;
+                currentPosition += 1;
+            }
+
+            if (currentPosition == index)
+            {
+                dataNode.NextNode = currentNode.NextNode;
+                currentNode.NextNode = dataNode;
+            }
+            return head;
+        }
+
+        public static int Contains(Node<int> head, int data)
+        {
+            var currentNode = head;
+            int indexOfData = 1;
+            while (currentNode != null && currentNode.Data != data)
+            {
+                indexOfData += 1;
+                currentNode = currentNode.NextNode;
+                if (currentNode == null)
+                {
+                    indexOfData = -1;
+                }
+            }
+            Console.WriteLine($"Index of {data} is {indexOfData}");
+            return indexOfData;
+
+
+        }
+
+        public static int ContainsIterative(Node<int> head, int data)
+        {
+            
+            int indexOfData = 1;
+            if (head == null) return -1;
+            //if (head != null && head.Data != data)
+            //{
+            //    indexOfData += ContainsIterative(head.NextNode, data);
+            //}
+            //if (head != null && head.Data == data)
+            //{
+            //    return indexOfData;
+            //}
+
+            return indexOfData;
+        }
     }
 }
